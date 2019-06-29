03f19d00edae6cf41639912b2aaf3dc2aeffe4a6
Tushar Sinha <32814062+sinhatushar@users.noreply.github.com>
2019-06-30 05:20:36 +0530

    Update Initialize.jl

--- src/BlossomV/Initialize.jl
+++ src/BlossomV/Initialize.jl
@@ -1,5 +1,4 @@
 Would support 2 types of initialization:
 1. Greedy initiation
-2. Fractional initialization [simplified 
-   version which uses heuristics(same as blossom 4
-   blossom 5 implementation]
+2. Fractional initialization [simplified version which uses heuristics(same as blossom 4 blossom 5 implementation)]
+
