5eb1457656f3896189781a0577cd62404fe5827a
Tushar Sinha <32814062+sinhatushar@users.noreply.github.com>
2019-06-29 10:54:43 +0530

    Create initialize file.

---
+++ src/BlossomV/Initialize.jl
@@ -0,0 +1,5 @@
+Would support 2 types of initialization:
+1. Greedy initiation
+2. Fractional initialization [simplified 
+   version which uses heuristics(same as blossom 4
+   blossom 5 implementation]
