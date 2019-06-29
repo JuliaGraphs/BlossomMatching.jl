deb2468ce306340373aa03c151f1e87d86bfc283
Tushar Sinha <32814062+sinhatushar@users.noreply.github.com>
2019-06-29 10:44:17 +0530

    Create controlling file.

---
+++ src/BlossomV/MinCostPerfectMatching.jl
@@ -0,0 +1,9 @@
+function min_cost_perfect_matching end
+# see https://github.com/mauro3/SimpleTraits.jl/issues/47#issuecomment-327880153 for syntax
+
+@traitfn function min_cost_perfect_matching(g::AG::(!IsDirected)) where {T<:Integer, AG <:AbstractGraph{T}}
+     Init()
+     UpdtatePrimal()
+     UpdateDual()
+     CheckComplementarySlacknessCondition()
+end
